90079eea079f0c68ab9a3f4dff257be44f2745e8
A J Blakie <ajblakie@tutanota.com>
2024-11-06 22:49:25 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!!!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
@@ -12,7 +12,8 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -20,11 +21,11 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!!"),
-            Ordering::Greater => println! ("Too big!!"),
+            Ordering::Greater => println!("Too big!!"),
             Ordering::Equal => {
                 println!("You win!!");
                 break;
